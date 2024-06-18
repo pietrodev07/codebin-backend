7acbdd3c262b2ffd56a2bee03d3b6d7dd0ce4ba1
pietrodev07 <pietro.dev.07@gmail.com>
2024-06-18 10:30:59 +0200

    feat(utils-routes): add generate-router util method

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,5 +1,15 @@
+import { ZodType } from "zod";
+
 export interface EmailOptions {
   to: string;
   subject: string;
   text: string;
 }
+
+interface RouteData {
+  method: "GET" | "POST" | "PUT" | "DELETE";
+  path: string;
+  validator?: ZodType;
+}
+
+export type RoutesData = Record<string, RouteData>;
