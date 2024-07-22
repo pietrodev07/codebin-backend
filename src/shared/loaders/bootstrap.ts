5794d42f1791bdc4430256d0c78470392d115c7c
pietrodev07 <pietro.dev.07@gmail.com>
2024-07-22 10:40:59 +0200

    feat(middlewares): add brief middleware

--- src/shared/loaders/bootstrap.ts
+++ src/shared/loaders/bootstrap.ts
@@ -3,13 +3,14 @@ import { cors } from "hono/cors";
 
 import { corsConfig } from "@/config/*";
 import { authRouter } from "@/auth/routes/auth.routes";
+import { brief } from "@/middlewares/brief.middleware";
 import { authMiddleware } from "@/middlewares/auth.middleware";
 import { snippetsRouter } from "@/snippets/routes/snippets.routes";
 
 export const bootstrapApplication = () => {
   const app = new Hono();
 
-  app.use(cors(corsConfig));
+  app.use(cors(corsConfig), brief(app));
 
   app.use("auth/me", authMiddleware);
   app.use("auth/logout", authMiddleware);
---
+++ src/shared/middlewares/brief.middleware.ts
@@ -0,0 +1,36 @@
+import { createMiddleware } from "hono/factory";
+import { HonoBase } from "hono/hono-base";
+
+const INFO_COLOR = "\x1b[36m";
+const RESET_COLOR = "\x1b[0m";
+
+let isExecuted = false;
+
+export const brief = (app: HonoBase, title: string = "APP ROUTES") => {
+  return createMiddleware(async (_, next) => {
+    if (!isExecuted) {
+      isExecuted = true;
+
+      const uniqueRoutes = app.routes.filter((value, index) => {
+        const _value = JSON.stringify(value);
+
+        return (
+          index ===
+          app.routes.findIndex((obj) => {
+            return JSON.stringify(obj) === _value;
+          })
+        );
+      });
+
+      const mappedRoutes = uniqueRoutes
+        .filter(({ method }) => method != "ALL")
+        .map(({ path, method }) => ({ path, method }));
+
+      console.info(`${INFO_COLOR}[INFO] ${title}:${RESET_COLOR}`);
+      console.table(mappedRoutes);
+      console.log();
+    }
+
+    await next();
+  });
+};
